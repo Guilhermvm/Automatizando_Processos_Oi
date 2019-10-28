20486e0e390e08776bad457b73e556d91fa6b533
Guilhermevm <guilherme.moreira@voxage.com.br>
2019-10-28 10:37:00 -0300

    Adicionando arquivos em Python

---
+++ Arquivos de Python/test_app_web_start.py
@@ -0,0 +1,13 @@
+from app_web_start import app_web_start
+
+
+def test_root_status():
+    instancia_app = app_web_start.app.test_client()
+    response = instancia_app.get('/devops_ac8')
+    assert response.status_code == 200, 'Deveria existir essa rota'
+
+
+def test_root_url():
+    instancia_app = app_web_start.app.test_client()
+    response = instancia_app.get('/devops_ac8')
+    assert response.data.decode('utf-8') == 'Index Page!', 'Essa rota existe'
